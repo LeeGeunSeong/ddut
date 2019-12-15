1f3a638a96890772354009e2ac81503c948c1167
sungwoncho94 <sungwoncho94@naver.com>
2019-12-16 08:57:15 +0900

    메인 페이지 기본 틀 완성

---
+++ frontend/src/components/Carousel.vue
@@ -0,0 +1,81 @@
+<template>
+  <div>
+    <b-carousel
+      id="carousel-1"
+      v-model="slide"
+      :interval="6000"
+      controls
+      indicators
+      background="#B0BEC5"
+      style="text-shadow: 1px 1px 2px #333;"
+      @sliding-start="onSlideStart"
+      @sliding-end="onSlideEnd"
+    >
+      <div id="slidediv">
+        <!-- Slides with img slot -->
+        <!-- Note the classes .d-block and .img-fluid to prevent browser default image alignment -->
+        <b-carousel-slide
+          caption="네이버 검색어 순위"
+          text="Nulla vitae elit libero, a pharetra augue mollis interdum."
+        >
+          <template v-slot:img>
+            <img
+              class="d-block w-100"
+              width="100vh"
+              src="https://picsum.photos/1024/480/?image=55"
+              alt="image slot"
+            />
+          </template>
+        </b-carousel-slide>
+        <b-carousel-slide
+          caption="다음 검색어 순위"
+          text="Nulla vitae elit libero, a pharetra augue mollis interdum."
+        >
+          <template v-slot:img>
+            <img
+              class="d-block w-100"
+              width="100vh"
+              src="https://picsum.photos/1024/480/?image=55"
+              alt="image slot"
+            />
+          </template>
+        </b-carousel-slide>
+      </div>
+    </b-carousel>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      slide: 0,
+      sliding: null
+    };
+  },
+  methods: {
+    onSlideStart() {
+      this.sliding = true;
+    },
+    onSlideEnd() {
+      this.sliding = false;
+    }
+  }
+};
+</script>
+
+<style scoped>
+div {
+  height: 76.5vh;
+  vertical-align: middle;
+}
+img {
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  position: absolute;
+  margin: auto;
+  opacity: 70%;
+}
+</style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,15 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import BootstrapVue from "bootstrap-vue";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+import Router from "vue-router";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.use(BootstrapVue);
+Vue.use(Router);
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  el: "#app",
+  render: h => h(App)
+}).$mount("#app");
