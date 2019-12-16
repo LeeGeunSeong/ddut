2e74e6c3988e0714c8c25cf0e5c05eb8e0659052
leegeunseong <skscp223@gmail.com>
2019-12-16 13:28:06 +0900

    cors 추가

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,5 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var app = express();
+
+app.all('/*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
